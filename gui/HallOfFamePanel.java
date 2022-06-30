1619778d3a35343082b05806ab0b9f9a68bb336f
sojos204 <67076560+sojos204@users.noreply.github.com>
2022-06-30 12:08:18 -0600

    Se implementa el Salón de la fama

--- gui/HallOfFamePanel.java
+++ gui/HallOfFamePanel.java
@@ -11,11 +11,22 @@ import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+
+import data.DataManager;
+import data.PersonData;
+
 import javax.imageio.ImageIO;
 
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.awt.*;
 import util.Util;
 
@@ -43,34 +54,22 @@ public class HallOfFamePanel extends JPanel{
         this.jlSalonFama.setForeground(Color.WHITE);
         this.add(this.jlSalonFama);
 
+        ArrayList<PersonData> temp = DataManager.getInstance().getRankings();
+        Object[] persons = temp.toArray();
+        sort(persons);
 
-        String[][] rec = {
-            { "Ranking", "Jugador","Tiempo"},
-            { "", "", ""},
-            { "1", "Steve", ""},
-            { "", "", ""},
-            { "2", "Virat", ""},
-            { "", "", ""},
-            { "3", "Kane", ""},
-            { "", "", ""},
-            { "4", "David", ""},
-            { "", "", ""},
-            { "5", "Ben", ""},
-            { "", "", ""},
-            { "6", "Eion", ""},
-            { "", "", ""},
-            { "7", "Kane", ""},
-            { "", "", ""},
-            { "8", "David", ""},
-            { "", "", ""},
-            { "9", "Ben", ""},
-            { "", "", ""},
-            { "10", "Eion", ""},
-         };
-        String[] header = { "Ranking", "Jugador","Tiempo"};
-        this.jtRanking = new JTable(rec, header);    
-        this.jtRanking.setFont(new java.awt.Font("Tahoma", 0, 20));
+    
+
+
+
+
+        DefaultTableModel tableModel = new DefaultTableModel();
+        this.jtRanking = new JTable(tableModel);    
+        this.jtRanking.setFont(new java.awt.Font("Tahoma", 0, 17));
         this.jtRanking.setBounds(370,150,250,350);  
+        tableModel.addColumn("Ranking");
+        tableModel.addColumn("Nombre");
+        tableModel.addColumn("Tiempo");
         this.jtRanking.setForeground(Color.WHITE);
         this.jtRanking.setBackground(new Color(0x007b00) );
         this.jtRanking.setGridColor(new Color(0x007b00) );
@@ -79,10 +78,20 @@ public class HallOfFamePanel extends JPanel{
         this.jtRanking.getColumnModel().getColumn(0).setCellRenderer(tcr);
         this.jtRanking.getColumnModel().getColumn(1).setCellRenderer(tcr);
         this.jtRanking.getColumnModel().getColumn(2).setCellRenderer(tcr);
+
+
+        for (int i = 0; i < persons.length; i++) {
+
+            tableModel.insertRow(i, new Object[] { i+1, ((PersonData) persons[i]).getName(), ((PersonData) persons[i]).getTime()});
+            if (i == 9) {
+                break;
+            }
+
+        }
         this.add(this.jtRanking);
 
         this.jlWinner = new JLabel();
-		this.jlWinner.setBounds(340, 65, 200, 250);
+		this.jlWinner.setBounds(340, 35, 200, 250);
 		try {
             this.buffWinner=ImageIO.read(getClass().getResourceAsStream("/resources/imgs/winner.png"));
             this.imageWinner = this.buffWinner.getScaledInstance(30, 30, this.imageWinner.SCALE_DEFAULT);
@@ -93,5 +102,17 @@ public class HallOfFamePanel extends JPanel{
 		this.jlWinner.setIcon(new ImageIcon(this.imageWinner));
 		this.add(this.jlWinner);
     }// initLabels
+
+    public void sort(Object[] persons) {
+        for (int j = 0; j < persons.length; j++) {
+            for (int i = 0; i < persons.length - 1; i++) {
+                if (((PersonData) persons[i]).getTime() > ((PersonData) persons[1 + i]).getTime()) {
+                    Object temp = persons[i];
+                    persons[i] = persons[i + 1];
+                    persons[i + 1] = temp;
+                }
+            }
+        }
+    }
 
 }
