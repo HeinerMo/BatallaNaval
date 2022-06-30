25b980862bcc529ea4fa119b53936f6b343bb923
Heiner <heinermongelopez@gmail.com>
2022-06-30 10:24:09 -0600

    Elimina lineas de prueba en el main

--- main/Main.java
+++ main/Main.java
@@ -3,27 +3,12 @@ package main;
 import gui.Window;
 import util.Util;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
-
-import javax.swing.JFrame;
-
-import data.DataManager;
-import data.PersonData;
-import engine.InputHandler;
-
 public class Main {
 
 	public static void main(String[] args) {
-		//Util.loadImages();
-		//Window window = new Window();
-		DataManager.getInstance().addToRanking("Heiner", 400f);
+		Util.loadImages();
+		Window window = new Window();
 
-		ArrayList<PersonData> temp = DataManager.getInstance().getRankings();
-
-		for (PersonData p: temp) {
-			System.out.println(p.getName() + " " + p.getTime());
-		}
 	}
 
 	
